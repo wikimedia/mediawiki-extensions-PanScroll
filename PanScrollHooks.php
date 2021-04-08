c30abd2847c5c5684ed702be62116841c2e60de2
zoranzoki21 <zorandori4444@gmail.com>
2021-04-08 23:01:46 +0200

    Add PHPCS and make it pass
    Change-Id: I1db0370ef4287a1df4e74565a0a0a238b382d1be

---
+++ PanScrollHooks.php
@@ -0,0 +1,49 @@
+<?php
+
+class PanScrollHooks {
+
+	/**
+	 * @param Parser &$parser
+	 */
+	public static function init( Parser &$parser ) {
+		$parser->setHook( 'panscroll', 'PanScrollHooks::renderFromTag' );
+	}
+
+	/**
+	 * @param string $input
+	 * @param array $args
+	 * @param Parser $parser
+	 * @param PPFrame $frame
+	 * @return string|bool
+	 */
+	public static function renderFromTag( $input, array $args, Parser $parser, PPFrame $frame ) {
+		$parser->getOutput()->addModules( 'ext.panscroll.core' );
+		$parsedInput = $parser->internalParse( $input );
+		$blockSize = self::getConfigArgs( $args );
+
+		$output = <<< EOT
+<div id="panscroll-element" class="panscroll-element" style="width: %s; height: %s">
+<div class="panscroll-container">
+<div class="panscroll-content">%s</div>
+</div>
+</div>
+EOT;
+		$output = sprintf( $output, $blockSize['width'], $blockSize['height'], $parsedInput );
+		return [ $output, 'noparse' => false, 'isHTML' => true ];
+	}
+
+	/**
+	 * @param array $args
+	 * @return array
+	 */
+	private static function getConfigArgs( array $args ) {
+		$size = [ 'width' => '100%', 'height' => '100%' ];
+		if ( array_key_exists( 'width', $args ) ) {
+			$size['width'] = $args['width'];
+		}
+		if ( array_key_exists( 'height', $args ) ) {
+			$size['height'] = $args['height'];
+		}
+		return $size;
+	}
+}
